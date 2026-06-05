2a5ffbbc218d48c830dea76cd3603e6eb93685bd
bishoy <marsaescapetours@gmail.com>
2026-06-06 00:30:30 +0300

    images

--- app/utils/image.ts
+++ app/utils/image.ts
@@ -4,5 +4,5 @@ export const getImageUrl = (path?: string | null) => {
   if (!path) return "/placeholder.png";
   if (path.startsWith("http")) return path;
 
-  return config.public.apiBase +  path;
+  return path;
 };
